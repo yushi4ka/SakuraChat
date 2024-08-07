1ca0f1200a0d04befc9611371f36fddc00af0ac5
yushi4ka <yushdark2424@gmail.com>
2024-08-07 17:24:54 +0500

    Update ColorUtil.java

--- src/main/java/org/winderdev/SakuraChat/Util/ColorUtil.java
+++ src/main/java/org/winderdev/SakuraChat/Util/ColorUtil.java
@@ -1,1 +1,25 @@
+package org.winterdev.SakuraChat.Util;
 
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ColorUtil {
+    public ColorUtil() {
+    }
+
+    public static String color(String message) {
+        Pattern pattern = Pattern.compile("&#([A-Fa-f0-9]{6})");
+
+        String color;
+        String replacement;
+        for(Matcher matcher = pattern.matcher(message); matcher.find(); message = message.replace("&#" + color, replacement)) {
+            color = matcher.group(1);
+            replacement = ChatColor.of("#" + color).toString();
+        }
+
+        message = message.replace("{n}", "\n");
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+}
