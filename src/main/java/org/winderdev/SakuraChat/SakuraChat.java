88aa64aed8fe4715c6a134f626060c65c08bc5f5
yushi4ka <yushdark2424@gmail.com>
2024-08-10 21:59:43 +0500

    Update SakuraChat.java

--- src/main/java/org/winderdev/SakuraChat/SakuraChat.java
+++ src/main/java/org/winderdev/SakuraChat/SakuraChat.java
@@ -9,15 +9,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.winterdev.SakuraChat.Commands.SChat;
 import org.winterdev.SakuraChat.Listeners.*;
 import org.winterdev.SakuraChat.Util.EmojiUtil;
+import org.winterdev.SakuraChat.Util.LangUtil;
 
 public final class SakuraChat extends JavaPlugin {
+
     private static SakuraChat plugin;
     private YamlConfiguration emojiConfig;
+    private YamlConfiguration messagesRUConfig;
+    private YamlConfiguration messagesENConfig;
 
     public SakuraChat() {
     }
 
     public void onEnable() {
+
         plugin = this;
 
         this.getConfig().options().copyDefaults();
@@ -25,8 +30,16 @@ public final class SakuraChat extends JavaPlugin {
 
         InputStream inputStream = this.getResource("emoji.yml");
         this.emojiConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(inputStream));
+        saveEmojiConfig();
+        inputStream = this.getResource("langs/ru.yml");
+        this.messagesRUConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(inputStream));
+        saveMessagesRUConfig();
+        inputStream = this.getResource("langs/en.yml");
+        this.messagesENConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(inputStream));
+        saveMessagesENConfig();
 
         EmojiUtil.init(this);
+        LangUtil.init(this);
 
         this.getCommand("schat").setExecutor(new SChat());
         PluginManager pluginManager = Bukkit.getPluginManager();
@@ -41,15 +54,34 @@ public final class SakuraChat extends JavaPlugin {
         return plugin;
     }
 
-    public void onDisable() {
+    public void saveEmojiConfig() {
         try {
-            this.saveDefaultConfig();
             this.emojiConfig.save(String.valueOf(this.getDataFolder()) + "/emoji.yml");
         } catch (Exception var2) {
             Exception e = var2;
-            this.getLogger().severe("Error saving config: " + e.getMessage());
+            this.getLogger().severe("Error saving emoji.yml: " + e.getMessage());
         }
+    }
 
+    public void saveMessagesRUConfig() {
+        try {
+            this.messagesRUConfig.save(String.valueOf(this.getDataFolder()) + "/langs/ru.yml");
+        } catch (Exception var2) {
+            Exception e = var2;
+            this.getLogger().severe("Error saving ru.yml: " + e.getMessage());
+        }
+    }
+
+    public void saveMessagesENConfig() {
+        try {
+            this.messagesENConfig.save(String.valueOf(this.getDataFolder()) + "/langs/en.yml");
+        } catch (Exception var2) {
+            Exception e = var2;
+            this.getLogger().severe("Error saving en.yml: " + e.getMessage());
+        }
+    }
+
+    public void onDisable() {
         this.getLogger().info("SakuraChat is disabled!");
     }
 }
